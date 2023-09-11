165d7e05c82ac5718a922ab9d61f4e05ab025b9c
Fernando Tagliaferro <4048111+fernandotag@users.noreply.github.com>
2023-09-11 15:12:18 -0300

    style: create default theme

---
+++ src/styles/global.ts
@@ -0,0 +1,37 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :root {
+    font-synthesis: none;
+    text-rendering: optimizeLegibility;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-text-size-adjust: 100%;
+  }
+
+  body {
+    background: ${(props) => props.theme.background};
+    min-height: 100vh;
+  }
+
+  body, input, textarea, button {
+    font: 1rem 'Baloo 2', sans-serif;
+    font-weight: 400;
+  }
+
+  textarea:focus, input:focus{
+    outline: none;
+  }
+
+  @media (max-width: 768px) {
+    html {
+      font-size: 84%;
+    }
+  }
+`
