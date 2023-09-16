6c9c7a9281a594a0317b339b4117522887fbc49f
Fernando Tagliaferro <4048111+fernandotag@users.noreply.github.com>
2023-09-16 15:46:06 -0300

    feat: adjust header component

--- src/componets/Header/styles.ts
+++ src/componets/Header/styles.ts
@@ -2,7 +2,7 @@ import { styled } from 'styled-components'
 import coverImage from '../../assets/header-cover.svg'
 
 export const HeaderContainer = styled.header`
-  width: 100vw;
+  width: 100%;
   height: 18.5rem;
   background: url(${coverImage});
   background-size: cover;
@@ -10,6 +10,11 @@ export const HeaderContainer = styled.header`
   background-position: center center;
 
   display: flex;
-  align-items: center;
+  align-items: flex-start;
   justify-content: center;
+
+  img {
+    width: 9.25rem;
+    margin-top: 4rem;
+  }
 `
